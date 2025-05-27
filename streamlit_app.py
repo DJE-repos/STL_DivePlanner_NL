8e0174aff0de1bc1210bc61a32ec8d0341e9d8cb
DJE-repos <70265360+DJE-repos@users.noreply.github.com>
2025-05-27 22:44:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 df = pd.DataFrame(
     [
-       {"time": 0, "depth": 0, "pressure":None}
+       {"time": 0, "depth": 0, "pressure":1}
    ]
 )
+
+
 edited_df = st.data_editor(df, num_rows="dynamic")
 
 favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
