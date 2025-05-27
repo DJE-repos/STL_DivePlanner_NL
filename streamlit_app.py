9ca1b39b310a5f099cf6a03774f5fbe48089e921
DJE-repos <70265360+DJE-repos@users.noreply.github.com>
2025-05-27 22:52:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame(
+df = pd.DataFrame(data=
     [
        {"time": 0, "depth": 0, "pressure":1}
    ]
 )
 
-
-edited_df = st.data_editor(df, num_rows="dynamic")
+st.data_editor(
+    df, 
+    column_config={
+        "time": st.column_config.NumberColumn(
+            "time (minutes)"
+            help="how do you plan to take from the previous depth untill this one?"
+            format="%d minutes"
+        )
+    }
+    num_rows="dynamic",
