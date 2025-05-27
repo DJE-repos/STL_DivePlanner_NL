37296c727b5fc7911b10baa8b12ca0042d244cd3
DJE-repos <70265360+DJE-repos@users.noreply.github.com>
2025-05-27 22:58:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,21 @@ st.data_editor(
     df, 
     column_config={
         "time": st.column_config.NumberColumn(
-            "time (minutes)",
+            "tijd (minuten)",
             help="how do you plan to take from the previous depth untill this one?",
-            format="%d minutes"
+            format="%d min"
+        ),
+        "depth": st.column_config.NumberColumn(
+            "diepte (m)",
+            help="what depth do you plan to go to?",
+            format="%d m"
+        ),
+        "pressure"= st.column_config.NumberColumn(
+            "druk (bar) (calculated)",
+            help="difference in pressure (bar)",
+            format="%.1f bar"
         )
+            
     },
     num_rows="dynamic",
     )
