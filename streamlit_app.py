3211193b054c1543f9e3e113c633b1cec39efedf
DJE-repos <70265360+DJE-repos@users.noreply.github.com>
2025-05-27 22:41:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,14 @@
-from __future__ import annotations
-
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-
-def add_c(new_df: pd.DataFrame | None = None):
-    if new_df is not None:
-        if new_df.equals(st.session_state["df"]):
-            return
-        st.session_state["df"] = new_df
-
-    df = st.session_state["df"]
-    df["c"] = df["a"] + df["b"]
-    st.session_state["df"] = df
-    st.experimental_rerun()
-
-
-if "df" not in st.session_state:
-    st.session_state.df = pd.DataFrame(
-        {"a": [1, 2, 3], "b": [4, 5, 6], "c": [None, None, None]}
-    )
-    add_c()
-
-
-editable_df = st.data_editor(
-    st.session_state["df"],
-    key="data",
-    column_config={"c": st.column_config.Column(disabled=True)},
-    hide_index=True,
+df = pd.DataFrame(
+    [
+       {"command": "st.selectbox", "rating": 4, "is_widget": True},
+       {"command": "st.balloons", "rating": 5, "is_widget": False},
+       {"command": "st.time_input", "rating": 3, "is_widget": True},
+   ]
 )
+edited_df = st.data_editor(df, num_rows="dynamic")
 
-add_c(editable_df)
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
