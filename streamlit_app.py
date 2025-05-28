0b7daa006ca42d5a619bdb562a208fae4eda93d5
DJE-repos <70265360+DJE-repos@users.noreply.github.com>
2025-05-28 08:42:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,39 +9,39 @@ if "df" not in st.session_state:
     )
    
 edited_df=st.data_editor(
-df, 
-column_config={
-    "time": st.column_config.NumberColumn(
-        "tijd (minuten)",
-        help="how do you plan to take from the previous depth untill this one?",
-        format="%d min",
-        default=0
-    ),
-    "depth": st.column_config.NumberColumn(
-        "diepte (m)",
-        help="what depth do you plan to go to?",
-        format="%d m",
-        default=0
-    ),
-    
-    "pressure": st.column_config.NumberColumn(
-        "druk (bar)",
-        help="difference in pressure (bar)",
-        format="%.1f bar",
-        disabled=True,
-        default=0
-    ),
-    "calculation": st.column_config.TextColumn(
-        "berekening",
-        default="diepte*tijd*verbruik=",
-        disabled=True
-    )
-    
+    st.session_state.df, 
+    column_config={
+        "time": st.column_config.NumberColumn(
+            "tijd (minuten)",
+            help="how do you plan to take from the previous depth untill this one?",
+            format="%d min",
+            default=0
+        ),
+        "depth": st.column_config.NumberColumn(
+            "diepte (m)",
+            help="what depth do you plan to go to?",
+            format="%d m",
+            default=0
+        ),
         
-},
-num_rows="dynamic",
-)
+        "pressure": st.column_config.NumberColumn(
+            "druk (bar)",
+            help="difference in pressure (bar)",
+            format="%.1f bar",
+            disabled=True,
+            default=0
+        ),
+        "calculation": st.column_config.TextColumn(
+            "berekening",
+            default="diepte*tijd*verbruik=",
+            disabled=True
+        )
+        
+            
+    },
+    num_rows="dynamic",
+    )
 
 if st.button("recalculate"):
-    edited_df["pressure"]=15
-    st.write(edited_df)
+    st.session_state.df["pressure"]=15
+    
