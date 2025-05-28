91958d08ee54838fe1d4a114b36775f68618e925
DJE-repos <70265360+DJE-repos@users.noreply.github.com>
2025-05-28 08:48:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,53 @@
 import streamlit as st
 import pandas as pd
 
+st.title("my awesome app")
 if "df" not in st.session_state:
     st.session_state.df = pd.DataFrame(data=
         [
            {"time": 0, "depth": 0, "pressure":1,"calculation":"diepte*tijd*verbruik=","air_used":0 }
        ]
     )
-   
-edited_df=st.data_editor(
-    st.session_state.df, 
-    column_config={
-        "time": st.column_config.NumberColumn(
-            "tijd (minuten)",
-            help="how do you plan to take from the previous depth untill this one?",
-            format="%d min",
-            default=0
-        ),
-        "depth": st.column_config.NumberColumn(
-            "diepte (m)",
-            help="what depth do you plan to go to?",
-            format="%d m",
-            default=0
-        ),
-        
-        "pressure": st.column_config.NumberColumn(
-            "druk (bar)",
-            help="difference in pressure (bar)",
-            format="%.1f bar",
-            disabled=True,
-            default=0
-        ),
-        "calculation": st.column_config.TextColumn(
-            "berekening",
-            default="diepte*tijd*verbruik=",
-            disabled=True
-        )
-        
+
+@st.fragment()
+def air_plan_table(run_every=10s):
+    edited_df=st.data_editor(
+        st.session_state.df, 
+        column_config={
+            "time": st.column_config.NumberColumn(
+                "tijd (minuten)",
+                help="how do you plan to take from the previous depth untill this one?",
+                format="%d min",
+                default=0
+            ),
+            "depth": st.column_config.NumberColumn(
+                "diepte (m)",
+                help="what depth do you plan to go to?",
+                format="%d m",
+                default=0
+            ),
             
-    },
-    num_rows="dynamic",
-    )
+            "pressure": st.column_config.NumberColumn(
+                "druk (bar)",
+                help="difference in pressure (bar)",
+                format="%.1f bar",
+                disabled=True,
+                default=0
+            ),
+            "calculation": st.column_config.TextColumn(
+                "berekening",
+                default="diepte*tijd*verbruik=",
+                disabled=True
+            )
+            
+                
+        },
+        num_rows="dynamic",
+        )
+air_plan_table
 
 if st.button("recalculate"):
     st.session_state.df["pressure"]=15
     st.markdown("test")
+
     
