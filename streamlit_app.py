748910a064b469d749bb5c7a9c8e1bfa45018e44
DJE-repos <70265360+DJE-repos@users.noreply.github.com>
2025-05-29 17:19:33 +0000

    fgd

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.title("Luchtplanner")
 if "df" not in st.session_state:
     st.session_state.df = pd.DataFrame(data=
         [
-           {"time": 0, "depth": 0, "negative_depth":0, "pressure":1,"delta_pressure":0,"calculation":"diepte*tijd*verbruik=","air_used":0 }
+           {"time": 0, "time_cumu":0, "depth": 0, "negative_depth":0, "pressure":1,"delta_pressure":0,"calculation":"diepte*tijd*verbruik=","air_used":0 }
        ]
     )
 if "test" not in st.session_state:
@@ -22,12 +22,19 @@ def air_plan_table():
     air_useage=st.session_state.air_useage
     edited_df=st.data_editor(
         st.session_state.df, 
-        column_order=("time", "depth","pressure","delta_pressure","calculation","air_used"),
+        column_order=("time", "time_cumu","depth","pressure","delta_pressure","calculation","air_used"),
         column_config={
             "time": st.column_config.NumberColumn(
                 "tijd",
                 help="how do you plan to take from the previous depth untill this one?",
                 format="%.1f min",
+                default=0
+            ),
+            "time_cumu": st.column_config.NumberColumn(
+                "tijd (cumu)",
+                help="how do you plan to take from the previous depth untill this one?",
+                format="%.1f min",
+                disabled=True,
                 default=0
             ),
             "depth": st.column_config.NumberColumn(
@@ -59,18 +66,22 @@ def air_plan_table():
     edited_df.negative_depth=edited_df.depth*-1
     edited_df.delta_pressure=abs(edited_df.pressure - edited_df.pressure.shift(1))
     i=0
+
     while i<len(edited_df.index):
         
         if edited_df.loc[i,"delta_pressure"]>0 and i>0:
             if edited_df.loc[i,"time"]==0:
                 edited_df.loc[i,"time"]=edited_df.loc[i,"delta_pressure"]
+        if i>0:
+            j=i-1
+            edited_df.loc[i,"time_cumu"]=edited_df.loc[j,"time_cumu"]+edited_df.loc[i,"time"]
 
         edited_df.loc[i,"calculation"]=str(edited_df.loc[i,"time"]) + " min *"+str(edited_df.loc[i,"pressure"])+ " bar *"+str(st.session_state.air_useage)+" l/min ="
         i+=1
     
     if st.button("Bereken", type="primary"):
-
         st.session_state.df=edited_df
         st.rerun()
-st.area_chart(st.session_state.df,x="time",y="negative_depth", height=180)
+
+st.area_chart(st.session_state.df,x="time_cumu",y="negative_depth", height=180)
 air_plan_table()
